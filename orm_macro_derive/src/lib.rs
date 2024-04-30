261cb53a3fb906cbf4708fab3f90c345d5a2b963
josueBarretogit <josuebarretogit@gmail.com>
2024-04-30 11:24:18 -0500

    changed email and fixed warnings

--- orm_macro_derive/src/lib.rs
+++ orm_macro_derive/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use quote::{format_ident, quote, ToTokens};
+use quote::{format_ident, quote};
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(GetRepository)]
